bbaa416fe9680318679112a50949003756857789
Tot1M-53 <thomas.riallin53@gmail.com>
2025-07-21 13:29:04 +0000

    Fix: Correction des versions de dépendances et de la configuration Vite

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@fullcalendar/core',
-        '@fullcalendar/daygrid',
-        '@fullcalendar/timegrid',
-        '@fullcalendar/interaction',
-        '@fullcalendar/react'
-      ]
-    }
-  }
 })
